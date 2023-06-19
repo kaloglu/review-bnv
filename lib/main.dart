e0abf57ac65fac8036d86bbf4c184d89021ec640
Sohail Orakzai <sohailokzz@gmail.com>
2023-06-19 10:56:41 +0500

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:cihan_app/presentation/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
